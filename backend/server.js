d70393e67ff648b27bab28432b63d3ae7923f8a2
Didier Nsengiyumva <nsedidier@gmail.com>
2025-02-13 01:01:00 +0200

    Update server.js 1

--- backend/server.js
+++ backend/server.js
@@ -18,7 +18,7 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-
+app.use('/dashboard', express.static('../'));
 const upload = multer({ dest: 'uploads/' });
 
 const logUnprocessedMessage = (message) => {
