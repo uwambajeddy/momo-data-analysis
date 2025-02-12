13cbbf21b78e95d27c841f82bddd5b4f7f763d1d
Didier Nsengiyumva <nsedidier@gmail.com>
2025-02-13 01:05:07 +0200

    Update server.js 1

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const path = require('path');
 const cors = require('cors');
 const dotenv = require('dotenv');
 const multer = require('multer');
@@ -18,7 +19,7 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use('/dashboard', express.static('../'));
+express.static(path.join(__dirname, '..'))
 const upload = multer({ dest: 'uploads/' });
 
 const logUnprocessedMessage = (message) => {
