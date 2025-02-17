acda27851d87ac465cfb23b618feba255ee2355c
Didier Nsengiyumva <nsedidier@gmail.com>
2025-02-17 21:08:33 +0200

    Update script.js 908

--- script.js
+++ script.js
@@ -131,8 +131,8 @@ document.querySelectorAll('.menu-item').forEach(item => {
             loadComponent('internet-voicebundles');
         } else if (text === 'payments to code holders') {
             loadComponent('payments-to-code-holders');
-        } else if (text === 'transfers to mobile number') {
-            loadComponent('transfers-to-mobile-number');
+        } else if (text === 'transfers to mobile numbers') {
+            loadComponent('transfers-to-mobile-numbers');
         }
        
     });
